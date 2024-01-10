52fb4c6f3528f1fe6b360c40db13b2e44a958529
marc <marc.danull@schukat.com>
2024-01-10 08:05:27 +0100

    login system yet w.o. cookies added

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt")
 const app = express();
-
+const userModel = require('./models/user')
 const PORT = process.env.PORT || 5000;
-const urlencodedParser = express.urlencoded({ extended: false })
-const jwt = require("jsonwebtoken");
+// for parsing incoming request-body before handling
+app.use(express.json());
+//cors is "Cross-Origin Resource Sharing"
+app.use(cors())
+
+const dbURI = 'mongodb://localhost:27017/users';
+mongoose.connect(dbURI);
+
+app.post('/register', (req, res) => {
+
+    const { username, email, password } = req.body;
+    bcrypt.hash(password, 10)
+        .then(hash => {
+            userModel.create({ username, email, password: hash })
+                .then(user => res.json(user))
+                .catch(err => res.json(err.message))
+        })
+
+})
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    userModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password, (err, same) => {
+                    if (same) {
+                        res.json(same);
+                    } else {
+                        res.json("the password doesn't match");
+                    }
+                })
+            }
+        })
+
+})
+app.listen(PORT, () => console.log(`Server is live! on ${PORT}`))
 
 
-app.use(express.json(), urlencodedParser);
-
-const dbURI = 'mongodb://127.0.0.1:27017'
-
-
-mongoose.connect(dbURI)
-    .then((res) => {
-        app.listen(PORT, () => console.log(`Server is live! on ${PORT}`))
-    })
-    .catch(err => console.log(err))
 
